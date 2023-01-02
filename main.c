88933fe567c982df5dae26fe47fd75bcaa55ebdb
ruzbeh.moini@gmail.com <ruzbeh.moini@gmail.com>
2023-01-02 21:32:09 +0330

    first attempt

---
+++ main.c
@@ -0,0 +1,28 @@
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int get_command(){
+//
+//}
+//
+//int main()
+//{
+//    char a[30];
+//        FILE * fp;
+//
+//        fp = fopen ("../root/file.txt", "r+");
+//        fprintf(fp, "%s %s %s %d", "We", "are", "in", 2012);
+//
+//        fclose(fp);
+//        fp = fopen("../root/file.txt","r+");
+//        fscanf(fp , "%s" , a);
+//        fclose(fp);
+//
+//        printf("%s", a);
+//
+//
+//
+//    return(0);
+//
+//}
